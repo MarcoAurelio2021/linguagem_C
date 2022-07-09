3aaa6b505ab3e97cf7df5fdb26168905db3e9be5
Marco Aurélio de Avila <marcoaurelio_dev@outlook.com>
2022-07-09 16:24:48 -0300

    ordenação de vetores

--- aula_concetuais/mapa.c
+++ aula_concetuais/mapa.c
@@ -3,19 +3,19 @@
 
 
 int main() {
-    int dividendo, divisor, quociente, resto;
+    int dividendo, divisor, quo, resto;
     
     printf("Informe o numero dividendo:\n");
     scanf("%d", &dividendo);
     printf("Informe o numero divisor:\n");
     scanf("%d", &divisor );
 
-    quociente = dividendo / divisor;
+    quo = dividendo / divisor;
 
     resto = dividendo % divisor;
 
-    printf("\nO quocioente e: %.d", quociente);
-    printf("\nO resto e: %d", resto);
+    printf("\nQuocioente: %.d", quo);
+    printf("\nResto: %d", resto);
 
     return 0;
 }
